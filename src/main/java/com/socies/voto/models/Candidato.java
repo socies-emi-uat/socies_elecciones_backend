005b9e5e77da6a5f17735f706c4a7be45f087389
CamikCM <camikko422@gmail.com>
2025-06-14 16:44:59 -0400

    fecha sin hora para F-Nacimiento

--- src/main/java/com/socies/voto/models/Candidato.java
+++ src/main/java/com/socies/voto/models/Candidato.java
@@ -1,8 +1,15 @@
 package com.socies.voto.models;
 
-import jakarta.persistence.*;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -26,7 +33,7 @@ public class Candidato extends BaseEntity {
     private String cedulaIdentidad;
 
     @Column(nullable = false)
-    private LocalDateTime fechaNacimiento;
+    private LocalDate fechaNacimiento;
 
     @Column(name = "foto_url")
     private String fotoUrl;
