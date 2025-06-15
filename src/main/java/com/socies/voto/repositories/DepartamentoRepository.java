74fae8388cfd82e5b7302f1f5aa44d5095f67ae8
Mona lisa <ronalddiazy@gmail.com>
2025-06-14 20:35:14 -0400

    Se agrego el JPA para departamento

---
+++ src/main/java/com/socies/voto/repositories/DepartamentoRepository.java
@@ -0,0 +1,10 @@
+package com.socies.voto.repositories;
+
+import com.socies.voto.models.Departamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface DepartamentoRepository extends JpaRepository<Departamento, Long> {
+    Optional<Departamento> findByNombre(String nombre);
+}
