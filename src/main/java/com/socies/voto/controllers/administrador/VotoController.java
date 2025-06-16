a224b35a9387e89f6c0b153207fb06cfedaff675
Mona lisa <ronalddiazy@gmail.com>
2025-06-16 12:55:14 -0400

    Controlador para votos agregado

---
+++ src/main/java/com/socies/voto/controllers/administrador/VotoController.java
@@ -0,0 +1,35 @@
+package com.socies.voto.controllers.administrador;
+
+import com.socies.voto.dtos.Voto.AVotoDTO;
+import com.socies.voto.services.VotoService;
+import com.socies.voto.utils.ResponseWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/administrador/votos")
+public class VotoController {
+    @Autowired
+    private VotoService votoService;
+
+    @GetMapping
+    public ResponseEntity<ResponseWrapper<List<AVotoDTO>>> findAll() {
+        List<AVotoDTO> votoDTOS = votoService.findAllAdmin();
+        ResponseWrapper<List<AVotoDTO>> votos = new ResponseWrapper<>(true, "Todos los votos obtenidos", votoDTOS);
+        return new ResponseEntity<>(votos, HttpStatus.OK);
+    }
+
+    @GetMapping("/id_voto}")
+    public ResponseEntity<ResponseWrapper<AVotoDTO>> findById(@PathVariable Long id_voto) {
+        AVotoDTO votoDTO = votoService.findByIdAdministrador(id_voto);
+        ResponseWrapper<AVotoDTO> voto = new ResponseWrapper<>(true, "Voto encontrado", votoDTO);
+        return new ResponseEntity<>(voto, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/socies/voto/controllers/votante/VVotoController.java
@@ -0,0 +1,42 @@
+package com.socies.voto.controllers.votante;
+
+import com.socies.voto.dtos.Voto.AVotoDTO;
+import com.socies.voto.dtos.Voto.UVotoCreateDTO;
+import com.socies.voto.dtos.Voto.UVotoDTO;
+import com.socies.voto.services.VotoService;
+import com.socies.voto.utils.ResponseWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/votante/votos")
+public class VVotoController {
+    @Autowired
+    private VotoService votoService;
+
+    @GetMapping
+    public ResponseEntity<ResponseWrapper<List<UVotoDTO>>> findAll() {
+        List<UVotoDTO> votoDTOS = votoService.findAllVotantes();
+        ResponseWrapper<List<UVotoDTO>> votos = new ResponseWrapper<>(true, "Todos los votos obtenidos", votoDTOS);
+        return new ResponseEntity<>(votos, HttpStatus.OK);
+    }
+
+    @GetMapping("/id_voto}")
+    public ResponseEntity<ResponseWrapper<UVotoDTO>> findById(@PathVariable Long id_voto) {
+        UVotoDTO votoDTO = votoService.findByIdVotante(id_voto);
+        ResponseWrapper<UVotoDTO> voto = new ResponseWrapper<>(true, "Voto encontrado", votoDTO);
+        return new ResponseEntity<>(voto, HttpStatus.OK);
+    }
+
+    @PostMapping("/votar")
+    public ResponseEntity<ResponseWrapper<UVotoDTO>> votar(@RequestBody UVotoCreateDTO uVotoCreateDTO) {
+        UVotoDTO uVotoDTO = votoService.save(uVotoCreateDTO);
+        ResponseWrapper<UVotoDTO> response = new ResponseWrapper<>(true, "Voto realizado sin ningun problema.", uVotoDTO);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+}
