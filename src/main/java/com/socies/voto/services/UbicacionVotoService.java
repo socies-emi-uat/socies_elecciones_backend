4391581abe45039a39ca213be5c35b6c366daf4a
CamikCM <camikko422@gmail.com>
2025-06-19 18:22:22 -0400

    bugfix servicio ubicacion y controlador agregado

--- src/main/java/com/socies/voto/services/UbicacionVotoService.java
+++ src/main/java/com/socies/voto/services/UbicacionVotoService.java
@@ -3,8 +3,11 @@ package com.socies.voto.services;
 import com.socies.voto.dtos.UbicacionVoto.UbicacionVotoCreateDTO;
 import com.socies.voto.dtos.UbicacionVoto.UbicacionVotoDTO;
 import com.socies.voto.dtos.UbicacionVoto.UbicacionVotoUpdateDTO;
+import com.socies.voto.exceptions.ResourceAlreadyExistsException;
 import com.socies.voto.exceptions.ResourceNotFoundException;
+import com.socies.voto.models.Municipio;
 import com.socies.voto.models.UbicacionVoto;
+import com.socies.voto.repositories.MunicipioRepository;
 import com.socies.voto.repositories.UbicacionVotoRepository;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,6 +18,7 @@ import org.springframework.stereotype.Service;
 public class UbicacionVotoService {
 
     @Autowired private UbicacionVotoRepository ubicacionVotoRepository;
+    @Autowired private MunicipioRepository municipioRepository;
 
     public List<UbicacionVotoDTO> findAll() {
         return ubicacionVotoRepository.findAll().stream()
@@ -42,19 +46,36 @@ public class UbicacionVotoService {
                             throw new ResourceNotFoundException("Ubicación ya existente");
                         });
 
+        // Validar existencia del municipio
+        Long municipioId = dto.getMunicipio().getId();
+        Municipio municipio =
+                municipioRepository
+                        .findById(municipioId)
+                        .orElseThrow(
+                                () -> new ResourceNotFoundException("El municipio no existe."));
+
         UbicacionVoto ubicacionVoto = new UbicacionVoto();
         ubicacionVoto.setNombreUbicacion(dto.getNombreUbicacion());
         ubicacionVoto.setDescripcionUbicacion(dto.getDescripcionUbicacion());
         ubicacionVoto.setLatitude(dto.getLatitude());
         ubicacionVoto.setLongitude(dto.getLongitude());
         ubicacionVoto.setDireccion(dto.getDireccion());
-        ubicacionVoto.setMunicipio(dto.getMunicipio());
+        ubicacionVoto.setMunicipio(municipio); // ahora asignando el municipio validado
 
         ubicacionVotoRepository.save(ubicacionVoto);
         return new UbicacionVotoDTO(ubicacionVoto);
     }
 
     public UbicacionVotoDTO update(Long id, UbicacionVotoUpdateDTO dto) {
+        ubicacionVotoRepository
+                .findByNombreUbicacion(dto.getNombreUbicacion())
+                .ifPresent(
+                        p -> {
+                            if (!p.getId().equals(id)) {
+                                throw new ResourceAlreadyExistsException(
+                                        "nombre de ubicacion ya existe.");
+                            }
+                        });
 
         UbicacionVoto ubicacionVoto =
                 ubicacionVotoRepository
@@ -63,13 +84,20 @@ public class UbicacionVotoService {
                                 () ->
                                         new ResourceNotFoundException(
                                                 "Ubicación de voto no encontrada."));
+        // Validar existencia del municipio
+        Long municipioId = dto.getMunicipio().getId();
+        Municipio municipio =
+                municipioRepository
+                        .findById(municipioId)
+                        .orElseThrow(
+                                () -> new ResourceNotFoundException("El municipio no existe."));
 
         ubicacionVoto.setNombreUbicacion(dto.getNombreUbicacion());
         ubicacionVoto.setDescripcionUbicacion(dto.getDescripcionUbicacion());
         ubicacionVoto.setLatitude(dto.getLatitude());
         ubicacionVoto.setLongitude(dto.getLongitude());
         ubicacionVoto.setDireccion(dto.getDireccion());
-        ubicacionVoto.setMunicipio(dto.getMunicipio());
+        ubicacionVoto.setMunicipio(municipio); // ahora asignando el municipio validado
 
         ubicacionVotoRepository.save(ubicacionVoto);
         return new UbicacionVotoDTO(ubicacionVoto);
