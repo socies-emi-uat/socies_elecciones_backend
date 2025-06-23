e3ce6bbdc64d9e4ce37600494dff351695e1c892
CamikCM <camikko422@gmail.com>
2025-06-23 18:26:52 -0400

    se agrego EleccionesPartidoController y se actualizo repositorios

--- src/main/java/com/socies/voto/repositories/CandidaturaRepository.java
+++ src/main/java/com/socies/voto/repositories/CandidaturaRepository.java
@@ -1,9 +1,14 @@
 package com.socies.voto.repositories;
 
 import com.socies.voto.models.Candidatura;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CandidaturaRepository extends JpaRepository<Candidatura, Long> {
     boolean existsByNombreCandidaturaAndPartidoIdAndProcesoElectoralId(
             String nombreCandidatura, Long partidoId, Long procesoElectoralId);
+
+    List<Candidatura> findByPartidoId(Long partidoId);
+
+    List<Candidatura> findByProcesoElectoralId(Long procesoElectoralId);
 }
